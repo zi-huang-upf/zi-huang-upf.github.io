2d4c67ea6774213c734dae37f888811d4e617a3d
zi-huang-upf <52612581+zi-huang-upf@users.noreply.github.com>
2023-04-15 00:17:58 +0200

    Add files via upload

--- js/life.js
+++ js/life.js
@@ -2,26 +2,39 @@ const qMinesweeper = document.getElementById('q-minesweeper');
 
 const aMinesweeper = document.getElementById('a-minesweeper');
 
-// Show hidden DIV on hover
-qMinesweeper.addEventListener('mouseover', function handleMouseOver() {
-    aMinesweeper.style.visibility = 'visible';;
-});
-// Hide when mouse is away    
-qMinesweeper.addEventListener('mouseout', function handleMouseOut() {
-    aMinesweeper.style.visibility = 'hidden';
-
+// Show hidden DIV on click
+qMinesweeper.addEventListener('click', function handleClick() {
+    aMinesweeper.style.visibility = 'visible';
 });
 
 const qDuolingo = document.getElementById('q-duolingo');
 
 const aDuolingo = document.getElementById('a-duolingo');
 
-// Show hidden DIV on hover
-qDuolingo.addEventListener('mouseover', function handleMouseOver() {
-    aDuolingo.style.visibility = 'visible';;
+// Show hidden DIV on click
+qDuolingo.addEventListener('click', function handleClick() {
+    aDuolingo.style.visibility = 'visible';
 });
-// Hide when mouse is away    
-qDuolingo.addEventListener('mouseout', function handleMouseOut() {
-    aDuolingo.style.visibility = 'hidden';
 
+const qClassical = document.getElementById('q-classical');
+
+const aClassical = document.getElementById('a-classical');
+
+// Show hidden DIV on click
+qClassical.addEventListener('click', function handleClick() {
+    aClassical.style.visibility = 'visible';
+});
+
+const qRavel = document.getElementById('q-ravel');
+
+const aRavel = document.getElementsByClassName('ravel');
+
+qRavel.addEventListener('mouseover', function handleMouseOver() {
+    for(var i = 0; i < aRavel.length; i++)
+        {aRavel[i].style.backgroundColor = "#b9c6c5";}
+});
+
+qRavel.addEventListener('mouseout', function handleMouseOut() {
+    for(var i = 0; i < aRavel.length; i++)
+        {aRavel[i].style.backgroundColor = "transparent";}
 });
