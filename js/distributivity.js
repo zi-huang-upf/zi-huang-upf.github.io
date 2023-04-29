dc5ee371ad887a1437f31ef364232c7016daa557
zi-huang-upf <52612581+zi-huang-upf@users.noreply.github.com>
2023-04-29 18:46:26 +0200

    Add files via upload
    update the blog post on distributivity

---
+++ js/distributivity.js
@@ -0,0 +1,19 @@
+// The code comes from https://alvarotrigo.com/blog/css-animations-scroll/
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+window.addEventListener("scroll", reveal);
