a8aec1be27ee348b26bcfc64143fc92e97662d29
Zi Huang <zi.huang@upf.edu>
2024-06-27 18:06:39 +0200

    Cross Stitch
    Added two more items to cross stitch page

--- js/cross_stitch.js
+++ js/cross_stitch.js
@@ -33,3 +33,39 @@ function showDivs2(n) {
   }
   x[slideIndex2-1].style.display = "block";  
 }
+
+var slideIndex3 = 1;
+showDivs3(slideIndex3);
+
+function plusDivs3(n) {
+  showDivs3(slideIndex3 += n);
+}
+
+function showDivs3(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides3");
+  if (n > x.length) {slideIndex3 = 1}
+  if (n < 1) {slideIndex3 = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex3-1].style.display = "block";  
+}
+
+var slideIndex4 = 1;
+showDivs4(slideIndex4);
+
+function plusDivs4(n) {
+  showDivs4(slideIndex4 += n);
+}
+
+function showDivs4(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides4");
+  if (n > x.length) {slideIndex4 = 1}
+  if (n < 1) {slideIndex4 = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex4-1].style.display = "block";  
+}
